3f5f5bf1fc58f0502f2ed6cbcddb16e116422757
Bhaargavi N <bhaargavi.n@tringapps.com>
2023-01-27 17:43:46 +0530

    Initial commit

---
+++ src/main/java/org/example/credit.java
@@ -0,0 +1,41 @@
+package org.example;
+import java.util.Scanner;
+public class credit implements Cloneable {
+    private String cname;
+    private Integer cno;
+    private Integer cnum = 501005463;
+    private Integer expdate;
+
+    public credit( String cname,Integer cno, Integer expdate) {
+        this.cname=cname;
+        this.cno=cno;
+        this.expdate=expdate;
+    }
+    void eq()
+    {
+        if(cnum.equals(cno)) {
+            System.out.println("Credit card number matched!");
+        }
+        else
+        {
+            System.out.println("OOPS! Credit card number doesn't match");
+        }
+    }
+    protected Object Clone() throws  CloneNotSupportedException
+    {
+        return super.clone();
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException{
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the credit card holder name:");
+        String cname = sc.next();
+        System.out.println("Enter the credit card number:");
+        Integer cno = sc.nextInt();
+        System.out.println("Enter the expiration date:");
+        Integer expdate = sc.nextInt();
+        credit c1=new credit(cname,cno,expdate);
+        credit c2=(credit)c1.clone();
+        c2.eq();
+    }
+}
